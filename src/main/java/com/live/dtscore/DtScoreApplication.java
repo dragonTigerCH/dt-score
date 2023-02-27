9d3e5af48de1c7411aa87439be9a7876b3d168ca
yong <yoho9908@gmail.com>
2023-02-27 10:51:22 +0900

    add

---
+++ src/main/java/com/live/dtscore/DtScoreApplication.java
@@ -0,0 +1,13 @@
+package com.live.dtscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtScoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtScoreApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/live/dtscore/test/web/TestController.java
@@ -0,0 +1,20 @@
+package com.live.dtscore.test.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@Slf4j
+public class TestController {
+
+
+    @GetMapping("/hello")
+    public String test(){
+        log.info("출입 test()");
+        return "Hello, React!";
+    }
+
+}
