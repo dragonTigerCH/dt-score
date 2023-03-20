171a128696fe39c1d4bc97a67ea9d493dc78b200
yong <yoho9908@gmail.com>
2023-03-20 10:39:26 +0900

    test

---
+++ src/main/java/com/live/dtscore/test/score/domain/model/Score.java
@@ -0,0 +1,20 @@
+package com.live.dtscore.test.score.domain.model;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Score {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long seq;
+
+}
